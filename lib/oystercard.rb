5bd4ec7ad64a72ca2deb574519b6282d47d27d6b
majdeddine <majda-jambouri@hotmail.fr>
2017-11-03 15:42:01 +0000

    add fare method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,7 +3,7 @@ require_relative "journey.rb"
 class Oystercard
 # Oystercards objects
 # attribute readers: they are methods, they return the instance variable that they share the name with
-  attr_reader :balance, :journeys , :journey
+  attr_reader :balance, :journeys , :journey, :station, :exitstation
   CREDIT_LIMIT = 120
   MINIMUM_FARE = 1
 
@@ -11,6 +11,8 @@ class Oystercard
     @balance = balance
     @journeys = []
     @journey = journey
+    @station = station
+    @exitstation = exitstation
   end
 
   def top_up(amount)
@@ -18,16 +20,27 @@ class Oystercard
     @balance += amount
   end
 
-  def touch_in(station_placeholder)
+  def touch_in(station_placeholder='none')
     raise "Balance less than the minimum fare" if insufficient_balance?
-    journey.entry_station(station_placeholder)
+    @station = station_placeholder
+    journey.entry(station_placeholder)
   end
 
-  def touch_out(exit_placeholder)
-    deduct
-    journey.exit_station(exit_placeholder)
+  def touch_out(exit_placeholder='none')
+    #deduct
+    @exitstation = exit_placeholder
+    fare(@station,@exitstation)
+    journey.exit(exit_placeholder)
     @journeys << journey.current
     # journey.complete
+  end
+
+  def fare(station1, station2)
+    if station1 = 'none' || station2 = 'none'
+      @balance -= 6 * MINIMUM_FARE
+    else
+      @balance -= MINIMUM_FARE
+    end
   end
 
 
@@ -47,8 +60,16 @@ private
     @balance < MINIMUM_FARE
   end
 
-  def deduct
-    @balance -= MINIMUM_FARE
-  end
+  # def deduct
+  #   @balance -= MINIMUM_FARE
+  # end
 
+  def fare(station1, station2)
+    if station1 = 'none' || station2 = 'none'
+      @balance -= 6 * MINIMUM_FARE
+    else
+      @balance -= MINIMUM_FARE
+    end
+  end
+  
 end
